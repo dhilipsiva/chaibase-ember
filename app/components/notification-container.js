aa3e7063266078978c39616d7706279c55eeed04
dhilipsiva <dhilipsiva@gmail.com>
2017-06-10 18:25:29 +0530

    Minor refactor for notifications

--- app/components/notification-container.js
+++ app/components/notification-container.js
@@ -3,5 +3,6 @@ import Ember from 'ember';
 const { Component, inject: { service } } = Ember;
 
 export default Component.extend({
-  classNames: ["notification-container"]
+  classNames: ["notification-container"],
+  notifications: service(),
 });
--- app/initializers/notifications.js
+++
@@ -1,8 +0,0 @@
-export function initialize(application) {
-  application.inject('component:notification-container', 'notifications', 'service:notifications');
-}
-
-export default {
-  name: 'notifications',
-  initialize
-};
